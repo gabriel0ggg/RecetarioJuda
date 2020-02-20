189ce0154f3df221501d9d4a5f54f3a4a19475ad
Gabriel Guevara Gutierrez <gabriel0ggg@gmail.com>
2020-02-20 11:26:54 -0600

    Initial Commit

---
+++ RecetarioJuda/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RecetarioJuda
+//
+//  Created by Gabriel Guevara on 20/02/20.
+//  Copyright © 2020 Gabriel Guevara Gutierrez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
